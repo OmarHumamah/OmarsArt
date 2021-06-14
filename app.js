3fbab71679f2578c0871a3bda221d99e0ff3a7d5
OmarHumamah <>
2021-06-14 14:21:10 +0000

    pop

--- app.js
+++ app.js
@@ -1,13 +1,104 @@
 var modal = document.getElementById("myModal"); 
-var img =  document.getElementById("01");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
-img.onclick = function(){
+function popup(){
+img.onclick = function (){
   modal.style.display ="block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
+}
+var img =  document.getElementById("l01");
+popup();
+
+var img =  document.getElementById("l02");
+popup();
+
+var img =  document.getElementById("l03");
+popup();
+
+var img =  document.getElementById("l04");
+popup();
+
+var img =  document.getElementById("l05");
+popup();
+
+var img =  document.getElementById("l06");
+popup();
+
+var img =  document.getElementById("l07");
+popup();
+
+// .........................................................
+
+var img =  document.getElementById("d01");
+popup();
+
+var img =  document.getElementById("d02");
+popup();
+
+var img =  document.getElementById("d03");
+popup();
+
+var img =  document.getElementById("d04");
+popup();
+
+var img =  document.getElementById("d05");
+popup();
+
+var img =  document.getElementById("d06");
+popup();
+
+var img =  document.getElementById("d07");
+popup();
+
+// .........................................................
+
+var img =  document.getElementById("p01");
+popup();
+
+var img =  document.getElementById("p02");
+popup();
+
+var img =  document.getElementById("p03");
+popup();
+
+var img =  document.getElementById("p04");
+popup();
+
+var img =  document.getElementById("p05");
+popup();
+
+var img =  document.getElementById("p06");
+popup();
+
+var img =  document.getElementById("p07");
+popup();
+
+// .........................................................
+
+var img =  document.getElementById("a01");
+popup();
+
+var img =  document.getElementById("a02");
+popup();
+
+var img =  document.getElementById("a03");
+popup();
+
+var img =  document.getElementById("a04");
+popup();
+
+var img =  document.getElementById("a05");
+popup();
+
+var img =  document.getElementById("a06");
+popup();
+
+var img =  document.getElementById("a07");
+popup();
+
 
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function(){
